5ff1366df63f6e2b24cb48b5ef1b4164e3e6142f
teddybear0430 <teddybear0430@hotmail.com>
2021-03-18 01:47:57 +0900

    ログインのviewを作った。

--- app/resources/views/back/auth/login.blade.php
+++ app/resources/views/back/auth/login.blade.php
@@ -1,1 +1,55 @@
-<p>ログイン画面</p>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <title>ログイン画面 | Laravel CMS</title>
+</head>
+<div class="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+      <div class="max-w-md w-full space-y-8">
+          <div>
+              <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">ログイン</h2>
+              <p class="mt-2 text-center text-sm text-gray-600">ID・パスワードを入力してください</p>
+
+              @if ($errors->any())
+                  <div class="py-1">
+                      <ul>
+                      @foreach ($errors->all() as $error)
+                          <li class="flex items-center py-1 text-red-600 justify-center">{{ $error }}</li>
+                      @endforeach
+                      </ul>
+                  </div>
+              @endif
+        </div>
+
+        {{-- url()->current() 現在のURL（/login）を返す --}}
+        <form class="mt-8 space-y-6" method="POST" action="{{ url()->current() }}">
+            @csrf 
+            <input type="hidden" name="remember" value="true">
+            <div class="rounded-md shadow-sm -space-y-px">
+                <div>
+                    <label for="name" class="sr-only">ユーザー名</label>
+                    <input name="name" type="input" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Email address">
+                </div>
+                <div>
+                    <label for="password" class="sr-only">パスワード</label>
+                    <input id="password" name="password" type="password" autocomplete="current-password" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Password">
+                </div>
+            </div>
+
+          <div>
+              <button type="submit" class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                  <span class="absolute left-0 inset-y-0 flex items-center pl-3">
+                      <!-- Heroicon name: solid/lock-closed -->
+                      <svg class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                          <path fill-rule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clip-rule="evenodd" />
+                      </svg>
+                  </span>
+                  ログイン
+              </button>
+          </div>
+        </form>
+    </div>
+</div>
--- app/resources/views/back/layouts/base.blade.php
+++ app/resources/views/back/layouts/base.blade.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
 <html lang="ja">
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-  <title>{{ isset($title) ? $title . ' | ' : '' }}Laravel CMS</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <title>{{ isset($title) ? $title . ' | ' : '' }}Laravel CMS</title>
 </head>
 <body>
     <div id="app">
@@ -13,12 +13,16 @@
           <div class="container px-6 mx-auto flex justify-between items-center">
               <a class="navbar-brand text-3xl" href="/">Laravel CMS</a>
               <nav>
-                  <a href="#" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
-                    aaaa
+                  <a href="{{ route('back.dashboard') }}" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+                    ダッシュボード
                   </a>
-                  <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white">
-                    aaaa
-                  </a>
+                  <a href="#" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white" onClick="(function(){
+                    document.getElementById('logout-form').submit();
+                    return false;
+                    })();">ログアウト</a>
+                  <form id="logout-form" method="POST" action="{{ route('logout') }}" style="display: none;">
+                      @csrf 
+                  </form>
               </nav>
           </div>
         </header>
